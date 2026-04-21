5b0169e5064c619025f6bfe1e11be55cce15f5fc
shabz5297 <pigloisblue@gmail.com>
2026-04-21 14:23:42 +0100

    Delete Part2 directory

--- Part2/app/src/main/java/com/example/safecheck/model/SafetyCheckSummary.java
+++
@@ -1,8 +0,0 @@
-package com.example.safecheck.model;
-
-public class SafetyCheckSummary {
-    public long checkId;
-    public String date;
-    public String vehicleRegistration;
-    public int defectCount;
-}
